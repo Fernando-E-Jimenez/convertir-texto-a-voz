55d112a90dfa60d200fa8ce19d34503bc70dfd89
Fernando E Jimenez <71264515+Fernando-E-Jimenez@users.noreply.github.com>
2023-02-13 20:33:18 -0300

    Delete pyttsx3.py

--- pyttsx3.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)  # Establece la voz en español
-
-text = "Hola, esto es una prueba de texto a voz"
-engine.say(text)
-engine.runAndWait()
